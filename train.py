92a2f493db454873864e0203032a55c96afe0ca9
IgorD-Uni <bfm528@york.ac.uk>
2026-05-15 13:43:10 +0100

    Add files via upload

--- train.py
+++ train.py
@@ -5,6 +5,9 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 import torch.optim as optim
+import torch.nn.functional as F
+import PIL
+
 # Load Data
 
 #Augment the data images to useful size and form
@@ -12,17 +15,24 @@ my_transform = torchvision.transforms.Compose([
     #Resize image
     torchvision.transforms.Resize((224,224)),
 
-    # Change output channel from 3(RGB COLOURS) to 1(Greyscale)
-    torchvision.transforms.Grayscale(num_output_channels = 1),
+    # Change output channel from 3(RGB COLOURS) to 1(Greyscale) (Disabled)
+    # torchvision.transforms.Grayscale(num_output_channels = 1),
+    PIL.ImageOps.autocontrast(),
+
+    
 
     #Change to tensor
-    torchvision.transforms.ToTensor()
+    torchvision.transforms.ToTensor(),
+
+
+    #Normalise image so that tensors and neural network works better on them, recommended values from https://www.geeksforgeeks.org/python/how-to-normalize-images-in-pytorch/
+    torchvision.transforms.Normalize(mean = [0.5, 0.5, 0.5], std = [0.5, 0.5, 0.5]) #mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]
 ])
 
 
 
 
-# Download training data from open datasets.
+# Download training data from open datasets and preprocess images
 training_data = torchvision.datasets.OxfordIIITPet(
     root="data",
     split='trainval',
@@ -81,12 +91,21 @@ labels_map = {
     36: "Yorkshire Terrier",
 }
 
-train_dataloader = DataLoader(training_data, batch_size=3, shuffle=True)
+train_dataloader = DataLoader(training_data, batch_size=4, shuffle=True)
 
-print("hello")
+device = torch.device("cpu")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+
+print(device)
+
+
 
 # Display image and label.
+
 train_features, train_labels = next(iter(train_dataloader))
+
+print(train_features.size())
 print(f"Feature batch shape: {train_features.size()}")
 print(f"Labels batch shape: {train_labels.size()}")
 img = train_features[0].squeeze()
@@ -94,72 +113,60 @@ label = train_labels[0]
 print(img.size())
 
 # Swap axes for channel to work
-#img = img.swapaxes(0,1)
-#img = img.swapaxes(1,2)
-
+img = img.swapaxes(0,1)
+img = img.swapaxes(1,2)
+print(img)
 print(f"Label: {label} which is a", labels_map[label.item()])
 # Gives black and white image (img[:,:,2]) if channel is 3
-print(img)
 plt.imshow(img, cmap='grey')
 plt.show()
+
 
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(224*224, 512, bias= True),
-            nn.ReLU(),
-            nn.Linear(512, 512, bias= True),
-            nn.ReLU(),
-            nn.Linear(512, 37, bias= True),
-        )
 
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
-    
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels= 12, kernel_size= 11) # Input img size+1 - kernel size 
+        self.conv2 = nn.Conv2d(in_channels=12, out_channels= 24, kernel_size= 8) # (128, 214, 214)
 
-network  = NeuralNetwork()
-print(network)
+        self.pool = nn.MaxPool2d(2, stride=2) # -->(128, 107, 107)
 
-#Pass in input data
-
-X = torch.rand(1, 224, 224)
-logits = network(X)
-pred_probab = nn.Softmax(dim=1)(logits)
-y_pred = pred_probab.argmax(1)
-print(f"Predicted class: {y_pred}")
+        self.inputlayer = nn.Linear(24 * 50 * 50, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.outlayer = nn.Linear(84, len(labels_map))
 
 
+    def forward(self, x): # x.size() = (3, 224, 224)
+        x = F.relu(self.conv1(x)) #-> (12, 214, 124)
+        x = self.pool(x) #-> (12, 107, 107)
+        x = F.relu(self.conv2(x)) #-> (24, 100, 100 )
+        x = self.pool(x) # -> (24, 50, 50)
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.inputlayer(x))
+        x = F.relu(self.fc2(x))
+        x = F.log_softmax(self.outlayer(x), dim=1)
+        return x
 
 
-optimizer = optim.SGD(network.parameters(), lr=0.001, momentum=0.9)
-criterion = nn.CrossEntropyLoss()
+network = NeuralNetwork()
+network.to(device)
 
+loss_function = nn.NLLLoss()
+optimizer = optim.Adam(network.parameters(), lr=0.001)
 
-for epoch in range(4):  # loop over the dataset multiple times
-
-    running_loss = 0.0
-    for i, data in enumerate(train_dataloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-
-        # zero the parameter gradients
+epochs = 0
+for epoch in range(epochs):
+    for images, labels in train_dataloader:
         optimizer.zero_grad()
 
-        # forward + backward + optimize
-        outputs = network(inputs)
-        loss = criterion(outputs, labels)
+        output = network(images)
+        loss = loss_function(output, labels)
+        
         loss.backward()
         optimizer.step()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 100 == 99:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 100:.3f}')
-            running_loss = 0.0
+    print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
+
 
 print('Finished Training')
